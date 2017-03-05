f88f9e90b0cde3f3bc2cc67509db59eed53d8594
Ophir LOJKINE <pere.jobs@gmail.com>
2017-03-05 18:14:05 +0300

    Create base repo

---
+++ README.md
@@ -0,0 +1,7 @@
+# elm-base-repo
+
+This repo contains an empty Elm module. It's ready to be forked.
+
+## test
+
+Just run `npm install && npm test`
---
+++ src/BaseRepo.elm
@@ -0,0 +1,23 @@
+module BaseRepo exposing (..)
+
+{-| Module description
+
+## Definition
+@docs MyType
+
+## Functions
+@docs myFunction
+-}
+
+
+{-| Type
+-}
+type alias MyType =
+    {}
+
+
+{-| Function that does nothing
+-}
+myFunction : MyType -> MyType
+myFunction x =
+    x
